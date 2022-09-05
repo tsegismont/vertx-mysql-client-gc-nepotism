f5ad4c2c70b367113d5a600dd270832452d2b625
Thomas Segismont <tsegismo@redhat.com>
2022-09-05 15:28:25 +0200

    Add enableMetrics sysprop
    Makes it easier to switch once the application is built.

--- src/main/java/org/example/client/CustomLauncher.java
+++ src/main/java/org/example/client/CustomLauncher.java
@@ -7,6 +7,8 @@ import io.vertx.micrometer.VertxJmxMetricsOptions;
 
 public class CustomLauncher extends Launcher {
 
+    private static final boolean ENABLE_METRICS = Boolean.getBoolean("enableMetrics");
+
     public static void main(String[] args) {
         new CustomLauncher().dispatch(args);
     }
@@ -14,7 +16,7 @@ public class CustomLauncher extends Launcher {
     @Override
     public void beforeStartingVertx(VertxOptions options) {
         options.setMetricsOptions(new MicrometerMetricsOptions()
-                .setEnabled(false)
+                .setEnabled(ENABLE_METRICS)
                 .setJmxMetricsOptions(new VertxJmxMetricsOptions()
                         .setEnabled(true)
                         .setStep(1)
